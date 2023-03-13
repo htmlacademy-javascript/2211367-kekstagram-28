30785a7295903946ca59d8f5d80c7d57ef12eb04
Никита Кутасов <kutasovvvvv@mail.ru>
2023-03-13 22:42:57 +0300

    finish task

--- js/main.js
+++ js/main.js
@@ -19,16 +19,28 @@ const DESCRIPTION = [
   'Прекрасно'
 ];
 
-const LIKES = [
-  15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122,123,124,125,126,127,128,129,130,131,132,133,134,135,136,137,138,139,140,141,142,143,144,145,146,147,148,149,150,151,152,153,154,155,156,157,158,159,160,161,162,163,164,165,166,167,168,169,170,171,172,173,174,175,176,177,178,179,180,181,182,183,184,185,186,187,188,189,190,191,192,193,194,195,196,197,198,199,200
-];
+
+const LIKES = Array(186).fill('').map((it, index)=> 15 + index);
 
 const COMMENTS = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
 
-const SIMILAR_USER_COUNT = 25;
+const NAMES = [
+  'Артем',
+  'Егор',
+  'Виталий',
+  'Анастасия',
+  'Полина',
+  'Сергей',
+  'Никита'
+];
+
 
 const getRandomInteger = (a, b) => {
   const lower = Math.ceil(Math.min(a, b));
@@ -39,15 +51,16 @@ const getRandomInteger = (a, b) => {
 
 const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
 
-const createUser = () => ({
-  ID: getRandomArrayElement(ID),
-  URL: getRandomArrayElement(URL),
-  DESCRIPTION: getRandomArrayElement(DESCRIPTION),
-  LIKES: getRandomArrayElement(LIKES),
-  COMMENTS: getRandomArrayElement(COMMENTS),
+const createUser = (id) => ({
+  id: id,
+  url: getRandomArrayElement(URL),
+  description: getRandomArrayElement(DESCRIPTION),
+  likes: getRandomArrayElement(LIKES),
+  comments: getRandomArrayElement(COMMENTS),
+  name: getRandomArrayElement(NAMES),
 
 });
 
-const similarUsers = Array.from({length: SIMILAR_USER_COUNT}, createUser);
+const similarUsers = Array.from(ID, createUser);
 
 console.log(similarUsers);
