ccde8386e2973e2b76ccc2926e777dc51e4c109e
Никита Кутасов <kutasovvvvv@mail.ru>
2023-03-13 21:35:57 +0300

    module4

---
+++ js/main.js
@@ -0,0 +1,53 @@
+const ID = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25
+];
+
+const URL = [
+  'img/avatar-1.svg',
+  'img/avatar-2.svg',
+  'img/avatar-3.svg',
+  'img/avatar-4.svg',
+  'img/avatar-5.svg',
+  'img/avatar-6.svg',
+
+];
+
+const DESCRIPTION = [
+  'Передо мной интересная фотография',
+  'Мне нравится эта фотография, потому что она точно передаёт настроение',
+  'Мне нравится',
+  'Отличная фотография',
+  'Прекрасно'
+];
+
+const LIKES = [
+  15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122,123,124,125,126,127,128,129,130,131,132,133,134,135,136,137,138,139,140,141,142,143,144,145,146,147,148,149,150,151,152,153,154,155,156,157,158,159,160,161,162,163,164,165,166,167,168,169,170,171,172,173,174,175,176,177,178,179,180,181,182,183,184,185,186,187,188,189,190,191,192,193,194,195,196,197,198,199,200
+];
+
+const COMMENTS = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+];
+
+const SIMILAR_USER_COUNT = 25;
+
+const getRandomInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
+
+const createUser = () => ({
+  ID: getRandomArrayElement(ID),
+  URL: getRandomArrayElement(URL),
+  DESCRIPTION: getRandomArrayElement(DESCRIPTION),
+  LIKES: getRandomArrayElement(LIKES),
+  COMMENTS: getRandomArrayElement(COMMENTS),
+
+});
+
+const similarUsers = Array.from({length: SIMILAR_USER_COUNT}, createUser);
+
+console.log(similarUsers);
