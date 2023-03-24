faa5a01a60bfb81948051eaab930e00745d1fa43
Keks <keks@htmlacademy.ru>
2023-03-24 19:22:44 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/data.js
@@ -0,0 +1,79 @@
+
+import { getRandomArrayElement } from './util.js';
+
+const ID = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25
+];
+
+const URL = [
+  'photos/1.jpg',
+  'photos/2.jpg',
+  'photos/3.jpg',
+  'photos/4.jpg',
+  'photos/5.jpg',
+  'photos/6.jpg',
+  'photos/7.jpg',
+  'photos/8.jpg',
+  'photos/9.jpg',
+  'photos/10.jpg',
+  'photos/11.jpg',
+  'photos/12.jpg',
+  'photos/13.jpg',
+  'photos/14.jpg',
+  'photos/15.jpg',
+  'photos/16.jpg',
+  'photos/17.jpg',
+  'photos/18.jpg',
+  'photos/19.jpg',
+  'photos/20.jpg',
+  'photos/21.jpg',
+  'photos/22.jpg',
+  'photos/23.jpg',
+  'photos/24.jpg',
+  'photos/25.jpg',
+];
+
+const DESCRIPTION = [
+  'Передо мной интересная фотография',
+  'Мне нравится эта фотография, потому что она точно передаёт настроение',
+  'Мне нравится',
+  'Отличная фотография',
+  'Прекрасно'
+];
+
+
+const LIKES = Array(186).fill('').map((it, index)=> 15 + index);
+
+const COMMENTS = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+
+];
+
+const NAMES = [
+  'Артем',
+  'Егор',
+  'Виталий',
+  'Анастасия',
+  'Полина',
+  'Сергей',
+  'Никита'
+];
+
+const createUser = (id) => ({
+  id: id,
+  url: URL[id - 1],
+  description: getRandomArrayElement(DESCRIPTION),
+  likes: getRandomArrayElement(LIKES),
+  comments: getRandomArrayElement(COMMENTS),
+  name: getRandomArrayElement(NAMES),
+
+});
+
+const similarUsers = Array.from(ID, createUser);
+
+export { similarUsers };
+
---
+++ 6/js/main.js
@@ -0,0 +1,3 @@
+import { similarUsers } from './data.js';
+import { renderThumbnails } from './miniature.js';
+renderThumbnails(similarUsers);
