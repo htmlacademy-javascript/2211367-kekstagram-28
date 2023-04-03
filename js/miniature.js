257268d87abb2bb88980ebaf440c91d68363d0a6
Никита Кутасов <kutasovvvvv@mail.ru>
2023-04-03 18:43:51 +0300

    module8-task1

--- js/miniature.js
+++ js/miniature.js
@@ -1,22 +1,33 @@
+import { showBigPictute } from './user-modal.js';
+
 const thumbnailTemplate = document
   .querySelector('#picture')
   .content.querySelector('.picture');
 
 const container = document.querySelector('.pictures');
 
-const createThumbnail = ({ comments, description, likes, url }) => {
+const createThumbnail = (picture) => {
+  const { comments, description, likes, url, id } = picture;
   const thumbnail = thumbnailTemplate.cloneNode(true);
-  const commentsLength = comments.split('.').filter((it) => Boolean(it)).length;
+  const commentsLength = comments.length;
+
+  thumbnail.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    showBigPictute(picture);
+    document.body.classList.add('modal-open');
+
+  });
 
   thumbnail.querySelector('.picture__img').src = url;
   thumbnail.querySelector('.picture__img').alt = description;
   thumbnail.querySelector('.picture__comments').textContent = commentsLength;
   thumbnail.querySelector('.picture__likes').textContent = likes;
-
+  thumbnail.dataset.thumbnailId = id;
   return thumbnail;
 };
 
 const renderThumbnails = (pictures) => {
+
   const fragment = document.createDocumentFragment();
   pictures.forEach((picture) => {
     const thumbnail = createThumbnail(picture);
