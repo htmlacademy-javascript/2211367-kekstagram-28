e8e5e74636c75602f78f9b7358122592bbcfe873
Никита Кутасов <kutasovvvvv@mail.ru>
2023-04-09 13:52:46 +0300

    module9-task2

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 import { similarUsers } from './data.js';
 import { renderThumbnails } from './miniature.js';
+import { controlUploadFile } from './user-form.js';
+import { showLoadOverlay } from './user-form.js';
 
 
 renderThumbnails(similarUsers);
-
+controlUploadFile();
+showLoadOverlay();
 
